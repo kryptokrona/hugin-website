9a3f49c56deae301613c060c99787cc6ad884f0b
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-05 23:01:36 +0100

    Added smooth scroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-smoothscroll",
     {
       resolve: "gatsby-source-filesystem",
       options: {
