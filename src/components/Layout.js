59937108533988ebf73a3a66d7114947bfb17384
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-06 12:10:15 +0100

    Added SEO

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import Navbar from "./Navbar";
 import  Footer from "./Footer";
-
+import SEO from './SEO'
 import "../theme/style.css"
 
 
 const Layout = ( { children } ) => {
     return(
         <>
+            <SEO/>
             <Navbar/>
             {children}
             <Footer/>
---
+++ src/components/SEO.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+
+const SEO = () => {
+    return(
+            <Helmet htmlAttributes>
+                <html lang="en" />
+                <title>Hugin Messenger</title>
+                <meta name="Kryptokrona" content="Hugin Messenger" />
+                <script async src="https://cdn.splitbee.io/sb.js"></script>
+                <link rel="icon" src="../images/favicon.png"  type="image/x-icon"></link>
+            </Helmet>
+    )
+}
+
+export default SEO
