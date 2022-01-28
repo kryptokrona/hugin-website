07b17af1db91f3f9198faf4bb935f5e230085680
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-28 22:35:35 +0100

    Fixing og:image size when sharing

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -12,12 +12,15 @@ const Seo = () => {
                         the possibility to read them."/>
             <meta name="theme-color" content="#1d1d1d"/>
             <meta name="robots" content="index, follow"/>
-            <meta name="google-site-verification" content="ko52AFJlworyGWhg_fZWyRbf4abZsVCfRabcUF_Nh3g" />
+            <meta name="google-site-verification" content="ko52AFJlworyGWhg_fZWyRbf4abZsVCfRabcUF_Nh3g"/>
             <link rel="canonical" href="http://hugin.chat/"/>
             <script defer data-domain="hugin.chat" src="https://plausible.io/js/plausible.js"></script>
             <link rel="icon" src="../images/favicon.ico" type="image/x-icon"></link>
+            <meta property="og:title" content=""/>
             <meta property="og:image"
                   content="https://user-images.githubusercontent.com/36674091/148423512-2c019bd6-6b32-45ce-b380-100632b00f0f.png"/>
+            <meta property="og:image:width" content="1200"/>
+            <meta property="og:image:height" content="627"/>
         </Helmet>
     )
 }
