4879a00e6cb1ad420158a2d9c79b82474c1a15ea
Swepool <36674091+Swepool@users.noreply.github.com>
2022-03-06 21:23:43 +0100

    Prepping faucet front

---
+++ Static/faucet/script.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('address')
+let submit = document.getElementById('btn')
+
+function enableBtn() {
+    submit.disabled = false;
+    submit.classList.add('enabled')
+}
+
+fetch('https://blocksum.org:8083/faucet/balance')
+    .then(res => res.json())
+    .then(data => {
+            if(data.balance === "ok") {
+                document.getElementById('balance').textContent = "Funds available 🥳"
+            } else document.getElementById('balance').textContent = "No funds available 😭"
+    })
+
+submit.addEventListener('click', async e => {
+    e.preventDefault()
+    let captcha = document.getElementById('g-recaptcha-response').value
+    let value = {
+        "address": input.value,
+        "captcha": captcha
+    }
+    let json = JSON.stringify(value)
+    await fetch("https://blocksum.org:8083/faucet/send", {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json
+    })
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data)
+
+            if(data.msg ==="recent") {
+                document.getElementById('status').textContent = "Someone recently claimed, please wait a few seconds 🚨"
+            } else if (data.msg === "claimed") {
+                document.getElementById('status').textContent = `You've already claimed 🚨`
+            } else if (data.msg === "sent") {
+                document.getElementById('status').textContent = `Sent ✅ ${data.tx}`
+            }
+
+        })
+
+})
